f4c77b1daa83163bcb76667393e270f6d02b19e1
davebro1031 <dwbroaddus@gmail.com>
2023-10-21 13:32:01 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <ChakraProvider>
+      <div>Hello</div>
+    </ChakraProvider>
+  )
+}
+
