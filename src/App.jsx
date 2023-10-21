255dad2c6b5f4e3c9de208ef0ff78d2e93e76416
davebro1031 <dwbroaddus@gmail.com>
2023-10-21 14:10:12 -0600

    refactor ChakraProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
 import './App.css'
+import Header from './components/Header'
 
 export default function App() {
 
   return (
-    <ChakraProvider>
+    <>
+      <Header />
       <div>Hello</div>
-    </ChakraProvider>
+    </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            header goes here
+        </div>
+    )
+}
+
+export default Header
