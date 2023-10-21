e418c4eb7ae1ba7fef1ab275d3cb12d438bcab94
davebro1031 <dwbroaddus@gmail.com>
2023-10-21 14:49:24 -0600

    begin simple header, searchbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Flex, Box, Heading, Spacer, ButtonGroup, Button } from '@chakra-ui/react'
+import Search from './Search'
 
 function Header() {
     return (
-        <div>
-            header goes here
-        </div>
+        <Flex gap="2" align='center' p='2' bg='purple'>
+            <Heading size='lg' color='white'>Nerd Snipes</Heading>
+            <Spacer />
+            <Search />
+            <ButtonGroup>
+                <Button colorScheme='purple'>Dave</Button>
+                <Button colorScheme='purple'>Edith</Button>
+
+            </ButtonGroup>
+        </Flex>
     )
 }
 
---
+++ src/components/Search.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Input } from '@chakra-ui/react'
+
+function Search() {
+    return (
+        <Input/>
+    )
+}
+
+export default Search
