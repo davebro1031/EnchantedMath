08b29e723d20d253e193aa1a3b2a2686bff03697
davebro1031 <dwbroaddus@gmail.com>
2023-10-22 13:53:10 -0600

    refactor chili slider into its own component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,32 +3,16 @@ import { VStack, Box, Button, Text, RangeSlider, RangeSliderTrack, RangeSliderFi
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPepperHot } from '@fortawesome/free-solid-svg-icons'
 import Search from './Search'
+import ChiliSlider from './ChiliSlider'
 
 function Sidebar({ setQuery, width, setChiliRange }) {
     return (
-        <VStack as="section" height='100%' width={width} bg='teal' position='fixed' left='0' gap='24px'>
-            <Box />
+        <VStack as="section" height='100%' width={width} bg='teal' position='fixed' left='0' gap='6' p='6'>
+            {/* <Box /> */}
             <VStack width='100%'>
                 <Search setQuery={setQuery} />
                 <Button> Advanced Search Options</Button>
-                <Box width={`calc(${width} - 3em)`}>
-                    <Text color='white'>Difficulty</Text>
-                    <RangeSlider
-                        onChangeEnd={(val) => setChiliRange(val)}
-                        aria-label={['min', 'max']}
-                        min={0}
-                        max={3}
-                        step={1}
-                        defaultValue={[0, 3]}>
-                        <RangeSliderTrack>
-                            <RangeSliderFilledTrack bg="red.700" />
-                        </RangeSliderTrack>
-                        <RangeSliderThumb index={0} _focus={{ boxShadow: '0 0 0 3px rgba(164, 40, 40, 0.6)' }} />
-                        <RangeSliderThumb index={1} boxSize={7} _focus={{ boxShadow: '0 0 0 3px rgba(164, 40, 40, 0.6)' }}>
-                            <FontAwesomeIcon icon={faPepperHot} />
-                        </RangeSliderThumb>
-                    </RangeSlider>
-                </Box>
+                <ChiliSlider setChiliRange={setChiliRange} />
             </VStack>
         </VStack>
     )
