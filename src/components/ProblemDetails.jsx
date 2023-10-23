69a7c074ad3ebff19fc0885f8387662a12aa3f9f
davebro1031 <dwbroaddus@gmail.com>
2023-10-23 09:35:49 -0600

    connect problem id to details page

--- src/components/ProblemDetails.jsx
+++ src/components/ProblemDetails.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { problems } from '../problems/data';
 
 export default function ProblemDetails() {
     const { id } = useParams();
+    const problem = problems.find(problem => problem.id = id)
+    
     return (
         <div>
-            This is problem #{id}
+            <div>
+                {problem.title}
+
+            </div>
+            <div>
+                {problem.text}
+            </div>
         </div>
     )
 }
