c00746d45fbd0de449aa829eb21ef2e3be229b1d
davebro1031 <dwbroaddus@gmail.com>
2023-10-23 09:41:56 -0600

    link problem cards to details pages

--- src/components/ProblemCard.jsx
+++ src/components/ProblemCard.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import { Card, CardBody, CardHeader, Text, Heading, Spacer, Flex } from '@chakra-ui/react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPepperHot } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom'
 
 export default function ProblemCard({ problem }) {
+    const navigate = useNavigate()
     return (
-        <Card width='300px' marginBottom="1.5em" variant='elevated' _hover={{boxShadow:'0 0 5px 3px #0BC5EA'}}>
+        <Card
+            onClick={() => navigate(`/${problem.id}`)}
+            width='300px'
+            marginBottom="1.5em"
+            variant='elevated'
+            _hover={{ boxShadow: '0 0 5px 3px #0BC5EA' }}>
             <CardHeader>
                 <Flex align='center'>
                     <Heading size='md'>{problem.title}</Heading>
@@ -22,10 +29,8 @@ export default function ProblemCard({ problem }) {
 
 const Chilis = ({ rating }) => {
     return (
-
         Array.from({ length: rating }, (_, i) => i).map(el => (
             <FontAwesomeIcon key={el} icon={faPepperHot} />
         ))
-
     )
 }
--- src/components/ProblemDetails.jsx
+++ src/components/ProblemDetails.jsx
@@ -5,12 +5,11 @@ import { problems } from '../problems/data';
 export default function ProblemDetails() {
     const { id } = useParams();
     const problem = problems.find(problem => problem.id = id)
-    
+
     return (
         <div>
             <div>
                 {problem.title}
-
             </div>
             <div>
                 {problem.text}
