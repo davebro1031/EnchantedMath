1ac5e233ea92a1a275e1b3a6928db2926663b4aa
davebro1031 <dwbroaddus@gmail.com>
2023-10-23 14:52:36 -0600

    move hints and see also to bottom of details page

--- src/components/Problem Details/ProblemDetails.jsx
+++ src/components/Problem Details/ProblemDetails.jsx
@@ -1,5 +1,5 @@
 import { useNavigate, useParams } from 'react-router-dom'
-import { Tabs, TabList, Tab, TabPanels, TabPanel, Heading, Box, Text, Spacer, Flex } from '@chakra-ui/react';
+import { Tabs, TabList, Tab, TabPanels, TabPanel, Heading, Box, Text, Spacer, Flex, VStack } from '@chakra-ui/react';
 import { problems } from '../../problems/data';
 import Hints from './Hints';
 import RelatedProblems from './RelatedProblems';
@@ -17,10 +17,10 @@ export default function ProblemDetails() {
 
 
     return (
-        <Box bg={'white'} p={3} m={4} minHeight='80vh'>
+        <Flex bg={'white'} p={3} m={4} minHeight='80vh' direction='column' >
             <Heading mb={3}> {problem.title}</Heading>
             <Heading size='md'> Difficulty: <Chilis rating={problem.chilis} /></Heading>
-            <Tabs colorScheme='red'>
+            <Tabs colorScheme='red' flexGrow={1} display={'flex'} flexDirection='column'>
                 <TabList>
                     {/* fontWeight='bold' fontSize='20px' */}
                     <Tab >Problem</Tab>
@@ -28,13 +28,12 @@ export default function ProblemDetails() {
                     <Tab >Feedback</Tab>
                 </TabList>
 
-                <TabPanels>
-                    <TabPanel height='100%' flexGrow={1} display={'flex'}>
-                        <Flex direction='column'>
+                <TabPanels flexGrow={1} display={'flex'} flexDirection='column'>
+                    <TabPanel flexGrow={1} display={'flex'} flexDirection='column' >
+                        <Flex direction='column' grow='1'>
                             <Text mb={3}>{problem.text}</Text>
                             <Spacer />
                             <Box>
-
                                 {problem.hints ? <Hints hints={problem.hints} /> : null}
                                 <RelatedProblems id={id} />
                             </Box>
@@ -48,6 +47,6 @@ export default function ProblemDetails() {
                     </TabPanel>
                 </TabPanels>
             </Tabs>
-        </Box>
+        </Flex>
     )
 }
