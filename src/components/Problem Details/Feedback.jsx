343a2ab3ab689a560bd0d940097fce53d0e6f92b
davebro1031 <dwbroaddus@gmail.com>
2023-10-25 09:36:35 -0600

    fix broken form

--- src/components/Problem Details/Feedback.jsx
+++ src/components/Problem Details/Feedback.jsx
@@ -1,74 +1,75 @@
 import { Field, Form, Formik } from 'formik';
 import { FormControl, FormLabel, Input, FormErrorMessage, Button, Select, Box, Textarea } from '@chakra-ui/react'
 
-export default function Feedback() {
-    function validateName(value) {
-        let error
-        if (!value) {
-            error = 'Name is required'
-        } else if (value.toLowerCase() !== 'naruto') {
-            error = "Jeez! You're not a fan 😱"
-        }
-        return error
-    }
+export default function Feedback({ id, title }) {
 
     return (
         <Box maxWidth='500px'>
             <Formik
-                initialValues={{ name: '' }}
-                onSubmit={(values, actions) => {
+                initialValues={{ user_email: '', topic: '', message: '' }}
+                validate={values => {
+                    const errors = {};
+                    if (values.user_email && !values.user_email.includes('@')) errors.user_email = 'Invalid email address';
+                    if (!values.topic || values.topic === "-- select one --") errors.topic = 'Select a topic';
+                    if (!values.message) errors.message = 'Cannot be blank'
+                    return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
                     setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2))
-                        actions.setSubmitting(false)
+                        // Make a copy of the form data
+                        const data = {}
+                        for (let key of Object.keys(values)) {
+                            data[key] = values[key]
+                        }
+
+                        // update the email data if needed
+                        !data.user_email && (data.user_email = "anonymous")
+
+                        // pass in the problem id as well
+                        data.id = id
+
+                        console.log(data)
+                        setSubmitting(false)
                     }, 1000)
                 }}
             >
                 {(props) => (
                     <Form>
-                        <Field name='name' validate={validateName}>
+                        <Field name='user_email' >
                             {({ field, form }) => (
-                                <FormControl isInvalid={form.errors.name && form.touched.name}>
-                                    <FormLabel>First name</FormLabel>
-                                    <Input {...field} placeholder='name' />
-                                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                                <FormControl isInvalid={form.errors.user_email && form.touched.user_email} >
+                                    <FormLabel>Your email (optional)</FormLabel>
+                                    <Input {...field} placeholder='someone@somewhere.com' focusBorderColor='teal.500' />
+                                    <FormErrorMessage>{form.errors.user_email}</FormErrorMessage>
                                 </FormControl>
                             )}
                         </Field>
 
-                        {/* <Field name='user_email' >
-                        {({ field, form }) => (
-                            <FormControl isInvalid={form.errors.name && form.touched.name} >
-                                <FormLabel>Your email (optional)</FormLabel>
-                                <Input {...field} placeholder='someone@somewhere.com' />
-                                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                            </FormControl>
-                        )}
-                    </Field> */}
+                        <Field name='topic' >
+                            {({ field, form }) => (
+                                <FormControl mt={4} isInvalid={form.errors.topic && form.touched.topic}>
+                                    <FormLabel>Subject</FormLabel>
+                                    <Select {...field} placeholder='-- select one --'>
+                                        <option value="suggested change">Suggest change</option>
+                                        <option value="new solution">New solution</option>
+                                        <option value="correction">Corrections</option>
+                                        <option value="comment">Comments</option>
+                                        <option value="miscellaneous">Other</option>
+                                    </Select>
+                                    <FormErrorMessage>{form.errors.topic}</FormErrorMessage>
+                                </FormControl>
+                            )}
+                        </Field>
 
-                        {/* <Field name='topic' >
-                        {({ field, form }) => (
-                            <FormControl mt={4} isInvalid={form.errors.name && form.touched.name} isRequired>
-                                <FormLabel>Subject</FormLabel>
-                                <Select {...field} placeholder='-- select one --'>
-                                    <option value="suggested change">Suggest change</option>
-                                    <option value="new solution">New solution</option>
-                                    <option value="correction">Corrections</option>
-                                    <option value="comment">Comments</option>
-                                    <option value="miscellaneous">Other</option>
-                                </Select>
-                            </FormControl>
-                        )}
-                    </Field> */}
-
-                        {/* <Field name='message' >
-                        {({ field, form }) => (
-                            <FormControl isInvalid={form.errors.name && form.touched.name} mt={4} isRequired>
-                                <FormLabel>Message</FormLabel>
-                                <Textarea {...field} placeholder='message' resize='vertical' />
-                                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                            </FormControl>
-                        )}
-                    </Field> */}
+                        <Field name='message' >
+                            {({ field, form }) => (
+                                <FormControl isInvalid={form.errors.message && form.touched.message} mt={4}>
+                                    <FormLabel>Message</FormLabel>
+                                    <Textarea {...field} placeholder='message' resize='vertical' focusBorderColor='teal.500' />
+                                    <FormErrorMessage>{form.errors.message}</FormErrorMessage>
+                                </FormControl>
+                            )}
+                        </Field>
 
 
                         <Button
@@ -82,7 +83,7 @@ export default function Feedback() {
                     </Form>
                 )}
             </Formik>
-        </Box>
+        </Box >
     )
 }
 
