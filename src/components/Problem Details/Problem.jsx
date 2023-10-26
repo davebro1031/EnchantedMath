9ffb0e94a618c883523c64ea380698d40b6810ce
davebro1031 <dwbroaddus@gmail.com>
2023-10-26 13:36:09 -0600

    use useBreakpointValue to clean up maxWidth code

--- src/components/Problem Details/Problem.jsx
+++ src/components/Problem Details/Problem.jsx
@@ -1,14 +1,19 @@
-import { Flex, Wrap, Text, Spacer, Box, Image } from "@chakra-ui/react"
+import { Flex, Wrap, Text, Spacer, Box, Image, useBreakpointValue } from "@chakra-ui/react"
 
 import Hints from "./Hints"
 import RelatedProblems from "./RelatedProblems"
 import Tags from './Tags';
 
 export default function Problem({problem, image}) {
+    const floatMaxWidth = useBreakpointValue({
+        base: '250px',
+        md: '300px',
+        lg: '350px'
+    })
     return (
         <Flex direction='column' grow='1'>
             <Box gap='4'>
-                {image ? <Image src={image.src} alt={image.alt} boxSize='100%' maxWidth={['',image.float?'250px':'500px',image.float?'300px':'500px']} float={['',image.float,image.float]} p='4'/> : null}
+                {image ? <Image src={image.src} alt={image.alt} boxSize='100%' maxWidth={image.float? floatMaxWidth:'500px'} float={['',image.float,image.float]} p='4'/> : null}
                 <Text mb={3}>{problem.text}</Text>
 
             </Box>
