448ddbfeda784eddcdb1b535d06ddb861525fbb3
davebro1031 <dwbroaddus@gmail.com>
2023-10-26 14:18:17 -0600

    add first two solutions to solutions.js

---
+++ src/problems/solutions.js
@@ -0,0 +1,19 @@
+export const solutions = [
+    {
+        id: 1,
+        answer: "112 cups of tropical punch",
+        solution: "The recipe calls for 3 cups of coconut milk, and because you have 30 cups, that means you have enough coconut milk for 10 batches.  However, in the same way, you only have enough orange juice for 8 batches.  Orange juice is the limiting ingredient (check the pineapple juice on your own), so we can only match a maximum of 8 batches if we follow the recipe.  Each batch makes 6 + 5 + 3 = 14 cups of punch, so we can make a total 112 cups of tropical punch.  There will be 6 cups of coconut milk and 22 cups of pineapple juice left over.",
+        links: null
+    },
+    {
+        id: 2,
+        answer: "33 minutes",
+        solution: "Cutting a log into 3 pieces means you're making TWO cuts.  If two cuts take 6 minutes, then each cut takes 3 minutes.  If you want to cut another log into 12 pieces, then you must make 11 cuts.  11 cuts at 3 minutes per cut gives 33 minutes for the whole task.",
+        links: [
+            {
+                to:"https://www.youtube.com/watch?v=hiCu-3YNSPQ",
+                text:"Mind Your Decisions (Youtube)"
+            }
+        ]
+    }
+]
