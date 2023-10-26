ec0d20ccecb40ce9cbd3be46b66a07bd6622699c
davebro1031 <dwbroaddus@gmail.com>
2023-10-26 14:59:10 -0600

    add tabheaders to solution

--- src/components/Problem Details/Solutions.jsx
+++ src/components/Problem Details/Solutions.jsx
@@ -1,14 +1,17 @@
+import { Box, Text, Heading } from '@chakra-ui/react'
+
+import TabHeader from './TabHeader'
 
 export default function Solutions({ solution }) {
     const solutions = solution.solutions
     return (
         <Box>
-            <Text>{solution.answer}</Text>
+            <Heading size='sm'>{["Answer:",solution.answer].join(" ")}</Heading>
             {solutions.map((solution, index) => {
                 return (
-                    <Box>
-                        <Text>{["Solution", solutions.length > 1 ? index : null].join(" ")}</Text>
-                        <Text>{solution}</Text>
+                    <Box key={index}>
+                        <TabHeader text={["Solution", solutions.length > 1 ? index : null].join(" ")} width={solutions.length>1?'7em':'5.5em'} />
+                        <Text mt='2'>{solution}</Text>
                     </Box>
 
                 )
