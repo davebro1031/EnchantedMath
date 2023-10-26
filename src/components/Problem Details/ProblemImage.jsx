1d4cdb9a983f6137ab32cb14e67d959cad96b660
davebro1031 <dwbroaddus@gmail.com>
2023-10-26 11:40:50 -0600

    remove ProblemImage

--- src/components/Problem Details/ProblemImage.jsx
+++
@@ -1,7 +0,0 @@
-import { Image } from "@chakra-ui/react"
-
-export default function ProblemImage({ image }) {
-    return (
-        <Image src={image.src} alt={image.alt} boxSize='30%' float='left' />
-    )
-}
