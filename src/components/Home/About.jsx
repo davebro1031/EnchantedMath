ba5641bbc928fc711cfeaaae8eaab9576407ce8a
davebro1031 <dwbroaddus@gmail.com>
2023-10-27 12:16:49 -0600

    add avatar to about page

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -1,8 +1,10 @@
-import { Box, Image } from "@chakra-ui/react"
+import { Box, Image, Avatar } from "@chakra-ui/react"
+import developer from '../../assets/developer.png'
 
 export default function About(){
     return (
-        <Box>
+        <Box bg={'white'} p={3} m={4} minHeight='80vh'>
+            <Avatar size='2xl' name='Dave Broaddus' src={developer}/>
             <Image src="https://imgs.xkcd.com/comics/nerd_sniping.png" />
         </Box>
     )
