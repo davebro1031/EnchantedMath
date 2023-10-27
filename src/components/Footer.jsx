6395fb4873625c998dd05b1b568f7aba44b66e2d
davebro1031 <dwbroaddus@gmail.com>
2023-10-27 17:28:32 -0600

    create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { HStack } from '@chakra-ui/react'
+
+export default function Footer() {
+    return (
+        <HStack>
+            <Box>Hi</Box>
+            <Box>this</Box>
+            <Box>is</Box>
+        </HStack>
+    )
+}
